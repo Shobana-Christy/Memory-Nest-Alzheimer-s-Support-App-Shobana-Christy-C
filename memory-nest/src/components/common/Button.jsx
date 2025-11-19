b70c3e7eac10a5bde57a5488cf05395787f6ce0e
Shobana Christy <shobichristy07@gmail.com>
2025-11-19 16:00:59 -0600

    changes added in Button

--- memory-nest/src/components/common/Button.jsx
+++ memory-nest/src/components/common/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ id, name, label, value, classes,onClick }) => {
 
     return (
-        <button id={id} name={name} value={value} className={classes} onClick={onClick}>
+        <button id={id} name={name} value={value} className={classes? classes: "button-class"} onClick={onClick}>
             {label}
         </button>
     );
